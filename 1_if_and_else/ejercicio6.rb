3b6e7fa7158ec7d97754a66075ad34e9c5bcdc49
Claudio Rivera <claudio.rivera.sh@gmail.com>
2018-06-05 20:10:27 -0400

    Solved Ex6

---
+++ 1_if_and_else/ejercicio6.rb
@@ -0,0 +1,8 @@
+# Utiliza álgebra booleana para hacer un refactoring de este código
+# Para verificar la respuestas, puedes modificar los valores de a y b.
+
+a = 'verdadero'
+b = 'falso'
+
+a == 'verdadero' ? (puts ':)') : (b == 'verdadero' ? (puts ':|') : (puts ':('))
+
