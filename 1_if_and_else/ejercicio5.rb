5e6d95fcf2b9b6539317fab1224b8b7282c53be4
Claudio Rivera <claudio.rivera.sh@gmail.com>
2018-06-05 20:10:21 -0400

    Solved Ex5

---
+++ 1_if_and_else/ejercicio5.rb
@@ -0,0 +1,8 @@
+# Utiliza álgebra booleana para hacer un refactoring de este código
+# Para verificar la respuestas, puedes variar los valores de a y b.
+
+a = true
+b = false
+
+a == true ? (b == true ? (puts 'Lograste A y B') : (puts 'Lograste ! Pero no B!')) : (puts 'No lograste A ni B')
+
